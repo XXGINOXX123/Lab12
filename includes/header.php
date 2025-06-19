841884986b6f9e20e98e65b157390225a8513160
cvas16 <166944720+cvas16@users.noreply.github.com>
2025-06-18 21:20:47 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,20 +4,24 @@
   <meta charset="UTF-8">
   <title>CYBERTEL</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet"> 
+  <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
   <div class="container-fluid">
-    <a class="navbar-brand" href="inicio.php">CYBERTEL</a>
-    <div class="collapse navbar-collapse">
+    <a class="navbar-brand" href="homepage.php">CYBERTEL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav me-auto">
-        <li class="nav-item"><a class="nav-link" href="inicio.php">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="homepage.php">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
         <li class="nav-item"><a class="nav-link" href="nosotros.php">Nosotros</a></li>
         <li class="nav-item"><a class="nav-link" href="contacto.php">Contacto</a></li>
         <li class="nav-item"><a class="nav-link" href="registro_cv.php">Registro CV</a></li>
         <li class="nav-item"><a class="nav-link" href="panel_rrhh.php">RRHH</a></li>
-        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
       </ul>
     </div>
   </div>
