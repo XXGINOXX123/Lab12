c191524f6525171e61774fbd3c77fab2f853ccac
cvas16 <166944720+cvas16@users.noreply.github.com>
2025-06-18 22:55:15 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,31 +1,31 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <title>CYBERTEL</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet"> 
-  <link rel="icon" href="img/imagen1.jpeg" sizes="32x32" type="image/jpeg">
-  <link rel="icon" href="img/imagen1.jpeg" sizes="16x16" type="image/jpeg">
-  <link href="css/style.css" rel="stylesheet">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">CYBERTEL</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
-        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
-        <li class="nav-item"><a class="nav-link" href="nosotros.php">Nosotros</a></li>
-        <li class="nav-item"><a class="nav-link" href="contacto.php">Contacto</a></li>
-        <li class="nav-item"><a class="nav-link" href="registro_cv.php">Registro CV</a></li>
-        <li class="nav-item"><a class="nav-link" href="panel_rrhh.php">RRHH</a></li>
-      </ul>
+  <!DOCTYPE html>
+  <html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <title>CYBERTEL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="icon" href="img/imagen1.jpeg" sizes="32x32" type="image/jpeg">
+    <link rel="icon" href="img/imagen1.jpeg" sizes="16x16" type="image/jpeg">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">CYBERTEL</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
+          <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+          <li class="nav-item"><a class="nav-link" href="nosotros.php">Nosotros</a></li>
+          <li class="nav-item"><a class="nav-link" href="contacto.php">Contacto</a></li>
+          <li class="nav-item"><a class="nav-link" href="registro_cv.php">Registro CV</a></li>
+          <li class="nav-item"><a class="nav-link" href="panel_rrhh.php">RRHH</a></li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-<div class="container">
+  </nav>
+  <div class="container">
