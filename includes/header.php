d2ae4cd208149a6291022ad163ee35d37bf06579
cvas16 <166944720+cvas16@users.noreply.github.com>
2025-06-18 21:55:15 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -12,13 +12,13 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
   <div class="container-fluid">
-    <a class="navbar-brand" href="homepage.php">CYBERTEL</a>
+    <a class="navbar-brand" href="index.php">CYBERTEL</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav me-auto">
-        <li class="nav-item"><a class="nav-link" href="homepage.php">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
         <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
         <li class="nav-item"><a class="nav-link" href="nosotros.php">Nosotros</a></li>
         <li class="nav-item"><a class="nav-link" href="contacto.php">Contacto</a></li>
