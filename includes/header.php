38edeadc0f33e794b24384be7910ebaa46d34c4c
santiagoErne <santiago3ernesto@gmail.com>
2025-06-20 11:28:32 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,33 +1,38 @@
- <!--Header-->
-  <!DOCTYPE html>
-  <html lang="es">
-  <head>
-    <meta charset="UTF-8">
-    <title>CYBERTEL</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="icon" href="img/imagen1.jpeg" sizes="32x32" type="image/jpeg">
-    <link rel="icon" href="img/imagen1.jpeg" sizes="16x16" type="image/jpeg">
-    <link href="css/style.css" rel="stylesheet">
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>CYBERTEL</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
+  <link rel="icon" href="img/imagen1.jpeg" sizes="32x32" type="image/jpeg">
+  <link rel="icon" href="img/imagen1.jpeg" sizes="16x16" type="image/jpeg">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg bg-white border-bottom mb-4">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">CYBERTEL</a>
+      <a class="navbar-brand text-dark" href="index.php">
+        <img src="img/imagen1.jpeg" width="50px" alt="Logo CYBERTEL">
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
-          <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
-          <li class="nav-item"><a class="nav-link" href="nosotros.php">Nosotros</a></li>
-          <li class="nav-item"><a class="nav-link" href="contactos.php">Contacto</a></li>
-          <li class="nav-item"><a class="nav-link" href="registro_cv.php">Registro CV</a></li>
-          <li class="nav-item"><a class="nav-link" href="panel_rrhh.php">RRHH</a></li>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link text-dark" href="index.php">Inicio</a></li>        
+          <li class="nav-item"><a class="nav-link text-dark" href="login.php">Login</a></li>
+          <li class="nav-item"><a class="nav-link text-dark" href="nosotros.php">Nosotros</a></li>
+          <li class="nav-item"><a class="nav-link text-dark" href="registro_cv.php">Registro CV</a></li>
+          <li class="nav-item"><a class="nav-link text-dark" href="panel_rrhh.php">RRHH</a></li>
+          <li class="nav-item">
+            <a class="btn btn-primary ms-3" href="contactos.php">Contáctanos</a>
+          </li>
         </ul>
       </div>
     </div>
   </nav>
+
   <div class="container">
-  <!--Header-->
