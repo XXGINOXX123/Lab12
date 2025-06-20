3c95a6c271938ffec523ae9f70ed730628244088
cvas16 <166944720+cvas16@users.noreply.github.com>
2025-06-20 18:42:11 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 </div>
-<footer class="text-center mt-4">
-  <hr>
+<footer class="footer-bg">
   <p>&copy; <strong>2025 CYBERTEL</strong></p>
 </footer>
+</div>
 <script src="js/scripts.js"></script>
 </body>
 </html>
